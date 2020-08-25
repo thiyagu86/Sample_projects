9c75752907253aedc090a2bf9790ab4cd5e4d9cc
thiyagu86 <send2thiyagu@gmail.com>
2020-08-25 19:09:53 +0530

    second version

--- LL.cpp
+++ LL.cpp
@@ -8,14 +8,46 @@ typedef struct node
   node* pNext;
 }node;
 
-node *listhead = NULL;
-node *listtail = NULL;
+
+node *listEvenHead = NULL;
+node *listEvenTail = NULL;
+node *listofMultipleHead = NULL;
+node *listofMultipleTail = NULL;
+node *listofOddHead = NULL;
+node *listofOddTail = NULL;
 
 void printList(std::string listName)
 {
     cout << "printing the value of list: " << listName;
+    node *listhead = NULL;
+    node *listtail = NULL;
+
+    //Choose a different list based on the input listname
+    if(listName == "evenList")
+    {
+	cout << "Entering even list" << endl;
+        listhead = listEvenHead;
+        listtail = listEvenTail;
+    }
+    else if(listName == "multipleList")
+    {
+	cout << "Entering multiple list" << endl;
+        listhead = listofMultipleHead;
+	listtail = listofMultipleTail;
+    }
+    else if (listName == "oddList")
+    {
+	cout << "Entering odd list" << endl;
+        listhead = listofOddHead;
+	listtail = listofOddTail;
+    }
+    else
+    {
+	cout << "Wrong list given" << endl;
+    }
     while(NULL != listhead)
     {
+	cout << "Thiyagu inside" << endl;
         cout << listhead->data << endl;
 	listhead = listhead->pNext; 
     }
@@ -24,6 +56,8 @@ void printList(std::string listName)
 bool addNodetoListFront(std::string listName,int data)
 {
     node *newElem = (node*)malloc(sizeof(node));
+    node *listhead = NULL;
+    node *listtail = NULL;
     //Choose a different list based on the input
     //Perform common action
     if(NULL == newElem)
@@ -43,12 +77,71 @@ bool addNodetoListFront(std::string listName,int data)
     listhead = newElem;
     return true;
 }
-
+bool readHead(std::string listName)
+{
+    node *listhead = NULL;
+    node *listhead = NULL;
+    //Choose a different list based on the input listname
+    if(listName == "evenList")
+    {
+	cout << "Entering even list" << endl;
+        listhead = listEvenHead;
+        listtail = listEvenTail;
+    }
+    else if(listName == "multipleList")
+    {
+	cout << "Entering multiple list" << endl;
+        listhead = listofMultipleHead;
+	listtail = listofMultipleTail;
+    }
+    else if (listName == "oddList")
+    {
+	cout << "Entering odd list" << endl;
+        listhead = listofOddHead;
+	listtail = listofOddTail;
+    }
+    else
+    {
+	cout << "Wrong list given" << endl;
+    }
+    if(NULL != listhead)
+    {
+	cout << "The top of the " <<listName<< " is: " << listhead->data << endl;  
+    }
+}
 bool addNodetoListBack(std::string listName,int data)
 {
+    node *listhead = NULL;
+    node *listtail = NULL;
+
     node *newElem = (node*)malloc(sizeof(node));
     //Choose a different list based on the input listname
-    //Perform common action of 
+    if(listName == "evenList")
+    {
+	//cout << "Entering even list for data" << data << endl;
+        listhead = listEvenHead;
+        listtail = listEvenTail;
+    }
+    else if(listName == "multipleList")
+    {
+	cout << "Entering multiple list" << endl;
+        listhead = listofMultipleHead;
+	listtail = listofMultipleTail;
+    }
+    else if (listName == "oddList")
+    {
+	cout << "Entering odd list" << endl;
+        listhead = listofOddHead;
+	listtail = listofOddTail;
+    }
+    else
+    {
+	cout << "Wrong list given" << endl;
+    }
+	 
+	
+    //
+    //Perform common action of adding element
     if(NULL == newElem)
     {
         return false;
@@ -65,12 +158,14 @@ bool addNodetoListBack(std::string listName,int data)
     newElem->pNext = NULL;
     listtail->pNext = newElem;
     listtail = newElem;
+    cout << "the elements of the even list is: " << endl;
+    printList("evenList");
     return true;
 }
 int main()
 {
-    cout << "Creating the even lists" << endl;
-    for(int i=1;i< 100;i++)
+    //cout << "Creating the even lists" << endl;
+    for(int i=1;i< 10;i++)
     {
 	if(i%2 == 0)
 	{
@@ -78,8 +173,8 @@ int main()
 	}
     }
 
-    printList("eventList");
-    cout << "the elements of the even list is: " << endl;
+    printList("evenList");
+    //cout << "the elements of the even list is: " << endl;
    
     cin.get(); 
     return 0;
